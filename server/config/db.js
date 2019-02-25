172922d271afd583351bf432b162095ce8ebf614
Ryan <100638060@oncampus.local>
2019-02-25 13:34:26 -0500

    reverting some changes back to local host mongo

--- server/config/db.js
+++ server/config/db.js
@@ -7,8 +7,8 @@ Web App Name:
 
 module.exports = {
   //local MongoDB deployment ->
-  //"URI": "mongodb://localhost/3000"
+  "URI": "mongodb://localhost/3000"
   //Remote MongoDB deployment -> let 
   //"URI" :"mongodb://thomas:123456@ds161059.mlab.com:61059/books308"
-  "URI" :"mongodb://ryan.s3:familyguy231@ds018258.mlab.com:18258/comp308-w2019-lesson4b"
+  //"URI" :"mongodb://ryan.s3:familyguy231@ds018258.mlab.com:18258/comp308-w2019-lesson4b"
 };
